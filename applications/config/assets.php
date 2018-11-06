1579eb1cd5761273e3a8f942da024c666eaf0cfc
Matthew Enderle <mattenderle488@gmail.com>
2018-11-06 11:37:36 +0300

    Added fonts to assets.php
    I needed to be able to load fonts that are stored locally instead of a CDN.

--- applications/config/assets.php
+++ applications/config/assets.php
@@ -1,10 +1,10 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-
 // Base path. URLs will be /path_base/path_<asset type>/<asset_name>
-$config['path_base'] 		    = 'assets';
+$config['path_base'] = 'assets';
 // Read above. Path for asset types.
-$config['path_js'] 		    = 'js';
-$config['path_css'] 	            = 'css';
-$config['path_img'] 		    = 'img';
+$config['path_js'] = 'js';
+$config['path_css'] = 'css';
+$config['path_img'] = 'images';
+$config['path_font'] = 'fonts';
+
 
